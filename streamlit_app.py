a164f763382dd55baa24b203037cf968112c2fad
jameschao79 <107243643+jameschao79@users.noreply.github.com>
2024-10-28 16:12:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-
+import pandas as pd 
 st.title('NyumbaniTechAI')
 
 st.write('This App is used to create machine language AI')
-
+df = pd.read_csv("https://raw.githubusercontent.com/jameschao79/cancer/refs/heads/main/Esophageal_Dataset.csv")
+df
